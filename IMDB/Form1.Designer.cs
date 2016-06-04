26e29d478d8e4d937c9f6a9b150c1c718ba5e4dd
shwetank1590 <shwetanksinghsrmscet@gmail.com>
2016-06-04 22:01:47 +0530

    Movie Rating
    Take Movie Path as input and retrievesmovie rating from IMDB site

--- IMDB/Form1.Designer.cs
+++ IMDB/Form1.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rating = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CountViewer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // listView1
@@ -40,15 +48,68 @@
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(818, 643);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Rating,
+            this.CountViewer});
+            this.dataGridView2.Location = new System.Drawing.Point(10, -2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(818, 651);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            // 
+            // Rating
+            // 
+            this.Rating.HeaderText = "Rating";
+            this.Rating.Name = "Rating";
+            // 
+            // CountViewer
+            // 
+            this.CountViewer.HeaderText = "Viewer";
+            this.CountViewer.Name = "CountViewer";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(854, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1041, 648);
+            this.ClientSize = new System.Drawing.Size(1159, 713);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.listView1);
-            this.Name = "Form1";
+            //this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +117,12 @@
         #endregion
 
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rating;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CountViewer;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- IMDB/Form1.cs
+++ IMDB/Form1.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Threading.Tasks;
 using System.Net;
-using System.Windows.Forms;
+
 
 namespace IMDB
 {
@@ -20,27 +22,106 @@ namespace IMDB
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //string str= 
-                //System.Diagnostics.Process.Start("http://google.com");
-            var url = "http://www.imdb.com/title/tt0944947/?ref_=fn_al_tt_1";
-            var textFromFile = (new WebClient()).DownloadString(url);
-            string str = textFromFile.ToString();
-            int location = str.IndexOf("<strong title=");
-            string temp=null;
-            if(location>0)
-            {
-                location++;
-                while(str[location]!='"')
-                {
-                    temp += str[location++];
-                }
-            }
+            
+
 
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private string Final(string result)
+        {
+           //// var url = "http://www.imdb.com/find?ref_=nv_sr_fn&q=game+of+thrones&s=all";
+            var textFromFile = (new WebClient()).DownloadString(result);
+            string str = textFromFile.ToString();
+            string searchfor = "<strong title=";
+            int location = 0;//str.IndexOf(searchfor);
+            string temp = null;
+            while (location < str.Length - searchfor.Length - 10 && !string.Equals(str.Substring(location, searchfor.Length), searchfor))
+                location++;
+            if (string.Equals(str.Substring(location, searchfor.Length), searchfor))
+            {
+                location = location + searchfor.Length + 1;
+                string va = str.Substring(location, 50);
+                while (str[location] != '"')
+                {
+                    temp += str[location++];
+                }
+                return temp;
+            }
+            return temp;
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str=null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                str = openFileDialog1.FileName;
+            }
+            //string temp = File.Path(str);
+            string directoryPath = Path.GetDirectoryName(str);
+            string[] FileName = Directory.GetFiles(directoryPath);
+            string[] FolderName = Directory.GetDirectories(directoryPath);
+            foreach(string F_name in FileName)
+            {
+                //string D_name = Path.GetFileName(F_name);
+                //string Fname = getres(D_name);
+                //dataGridView2.Rows.Add(D_name, Fname, null);
+            }
+
+            foreach (string F_name in FolderName)
+            {
+               
+                string D_name=Path.GetFileName(F_name);
+                string Fname = getres(D_name);
+                dataGridView2.Rows.Add(D_name, Fname, null);
+            }
+
+
+        }
+
+        public string getres(string full)
+        {
+            //string str= 
+            //System.Diagnostics.Process.Start("http://google.com");
+            // var url = "http://www.imdb.com/title/tt0944947/?ref_=fn_al_tt_1";
+            full = full.Replace(' ', '+').Replace('\t', '+').Replace('\n','+').Replace('.', '+');
+            //var url = "http://www.imdb.com/find?ref_=nv_sr_fn&q=game+of+thrones&s=all";
+            var url = "http://www.imdb.com/find?ref_=nv_sr_fn&q=" + full + "&s=all";
+            var textFromFile = (new WebClient()).DownloadString(url);
+            string str = textFromFile.ToString();
+            string searchfor = "<strong title=";
+            int location = 0;//str.IndexOf(searchfor);
+            string res = "http://www.imdb.com/";
+            string temp = null;
+
+            location = 0;
+            string result = "<tr class";
+            while (location < str.Length - result.Length - 10 && !string.Equals(str.Substring(location, result.Length), result))
+                location++;
+            string result1 = "<a href=";
+            while (location < str.Length - result1.Length - 10 && !string.Equals(str.Substring(location, result1.Length), result1))
+                location++;
+            location = location + 1 + result1.Length;
+            while (location < str.Length - result1.Length - 10 && str[location] != '"')
+                temp += str[location++];
+            if(temp!=null)
+            temp=temp.Replace(' ', '+').Replace('\t', '+').Replace('\n', '+').Replace('.', '+');
+            res += temp;
+            if(temp == null)
+                return null;
+           return Final(res);
         }
     }
 }
